e1ed4f832fb0b0c6dadf990ed27f8dc8b30f9aa3
Thorvaldur <thorvaldur.saemundsson@knowit.se>
2019-11-04 17:37:15 +0100

    made label slightly smaller

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+    import React from 'react';
 
 export const Label: React.FC<{}> = ({children}) => {
-    return <label style={{display:'inline-block', minWidth:'160px'}}>{children}</label>;
+    return <label style={{display:'inline-block', minWidth:'140px'}}>{children}</label>;
 }
 
