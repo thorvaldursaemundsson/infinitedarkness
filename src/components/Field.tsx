ff16522841bf3a1249d3671f2d581c28339d25e5
Thorvaldur <thorvaldur.saemundsson@knowit.se>
2019-11-04 17:37:02 +0100

    added dice rolling

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Paper, Button } from '@material-ui/core';
 import { Label } from './Label';
 
@@ -11,13 +11,24 @@ interface FieldProps {
     onChange: (n: number) => void;
 }
 
+
+const rand = (min:number,max:number) => {
+    return Math.round(Math.random()*(max-min) + min);
+};
+
+const d10 = () => rand(1,10);
+//results[0] + ' + ' + results[1] + ' ' + 
 export const Field: React.FC<FieldProps> = ({ label, value, onChange, max, min, modifier }) => {
+    const [results, setResults] = useState([0,0]);
     let ma = max !== undefined ? max : 10;
     let mi = min !== undefined ? min : 1;
     let total = modifier !== undefined ? value + modifier : value;
     return <Paper>
-        <Label>{label}: {value} {modifier !== undefined ? '+ ' + modifier + ' = ' + total : null}</Label>
+        <Label>{label}:</Label> <Label>{value} {modifier !== undefined ? ` | ${modifier} | ${total}` : null}</Label>
         <Button onClick={() => onChange(ma <= value ? value : value + 1)}>+</Button>
         <Button onClick={() => onChange(mi >= value ? value : value - 1)}>-</Button>
+        <Button onClick={() => setResults([d10(),d10()])}>&</Button>
+        {results[0] !== 0 ? `${total} + ${results[0]} + ${results[1]} = ${total+results[0]+results[1]}`
+        : null}
     </Paper>
 }
