1e350f5cdb1ae1624e330e17d64b16b3c455ac39
Thorvaldur <thorvaldur.saemundsson@knowit.se>
2019-11-04 16:56:19 +0100

    Updates

---
+++ precache-manifest.66dcd12b042e28415687c3842efabee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b141ca91ef0e94388cb7116b32a190",
+    "url": "../infinitedarkness/index.html"
+  },
+  {
+    "revision": "9899af94a013ca8fd1b5",
+    "url": "../infinitedarkness/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "53d00bdeb1f2dd16499e",
+    "url": "../infinitedarkness/static/js/2.d1b1d0bf.chunk.js"
+  },
+  {
+    "revision": "9899af94a013ca8fd1b5",
+    "url": "../infinitedarkness/static/js/main.920d7c0e.chunk.js"
+  },
+  {
+    "revision": "97788f2b6d4eb0d6e64e",
+    "url": "../infinitedarkness/static/js/runtime-main.c74a6b91.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "infinitedarkness/precache-manifest.e5dc627cd6686d66cbd19aa21d492556.js"
+  "../infinitedarkness/precache-manifest.66dcd12b042e28415687c3842efabee6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("infinitedarkness/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("../infinitedarkness/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
