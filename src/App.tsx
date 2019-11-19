0f8d78f02b57160961525724e2ba2a4d0be5360e
Thorvaldur <thorvaldur.saemundsson@knowit.se>
2019-11-19 17:16:45 +0100

    added bare bones player creation manual

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { CharacterSheet } from './components/CharacterSheet';
 import './App.css';
 import { Character } from './components/Character';
 import { Button, DialogTitle } from '@material-ui/core';
+import PlayerManual from './components/PlayerManual';
 
 const App: React.FC = () => {
   const [viewMode, setViewMode] = useState('main');
@@ -25,8 +26,8 @@ const App: React.FC = () => {
       </Conditional>
       <Conditional shouldView={viewMode === 'player manual'}>
         <DialogTitle>Player manual</DialogTitle>
-        This page will contain information on how to create a character, how to use the character sheet and the basic rules of the game
         <MainButton />
+        <PlayerManual />
       </Conditional>
       <Conditional shouldView={viewMode === 'game master'}>
         <DialogTitle>Game master</DialogTitle>
@@ -44,12 +45,15 @@ const App: React.FC = () => {
         <MainButton />
       </Conditional>
       <Conditional shouldView={viewMode === 'about'}>
-        <DialogTitle>About</DialogTitle>
-        This is a work in progress.
-        <div>Author: Thorvaldur Saemundsson</div>
-        <div>Technology: ReactJS, github pages</div>
-        <div>Project: Infinite Darkness, a sci-fi and fantasy game</div>
-        <div>Please forumlate any complaints and suggestions in the form of a pull request ;)</div>
+        <div style={{textAlign:'left', padding: '50px'}}>
+          <h2>About</h2>
+          <h3>This is a work in progress</h3>
+          <p>Author: Thorvaldur Saemundsson</p>
+          <p>Technology: ReactJS, github pages</p>
+          <p>Project: Infinite Darkness, a sci-fi and fantasy game</p>
+          <p>Please formulate any complaints and suggestions in the form of a pull request ;)</p>
+          <span style={{float:'right', fontSize:'11px'}}>Copyright Thorvaldur Saemundsson</span>
+        </div>
         <MainButton />
       </Conditional>
     </div>
