3e73a671016963a6feb4ae4aaa75714229cad261
Thorvaldur <thorvaldur.saemundsson@knowit.se>
2019-11-19 12:36:54 +0100

    added more options

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,52 @@ import { Character } from './components/Character';
 import { Button } from '@material-ui/core';
 
 const App: React.FC = () => {
-  const [viewMode,setViewMode] = useState('main');
+  const [viewMode, setViewMode] = useState('main');
   let character = new Character();
+  let options = ['character',
+    'player manual',
+    'game master',
+    'world & lore'];
 
   return (
     <div className="App">
-      {viewMode === 'main' ? <Button onClick={() => setViewMode('character')}>View Character Sheet</Button> : <CharacterSheet characterCallback={(c) => {character = c; setViewMode('main');}} initialCharacter={character}></CharacterSheet>}
+      <Conditional shouldView={viewMode === 'main'}>
+        <Menu callback={(option) => setViewMode(option)} options={options} />
+      </Conditional>
+      <Conditional shouldView={viewMode === 'character'}>
+        <CharacterSheet characterCallback={(c) => { character = c; setViewMode('main'); }} initialCharacter={character} />
+      </Conditional>
+      <Conditional shouldView={viewMode === 'player manual'}>
+        player manual, work in progress...
+        <Button onClick={() => setViewMode('main')}>X</Button>
+      </Conditional>
+      <Conditional shouldView={viewMode === 'game master'}>
+        game master, work in progress...
+        <Button onClick={() => setViewMode('main')}>X</Button>
+      </Conditional>
+      <Conditional shouldView={viewMode === 'world & lore'}>
+        world & lore, work in progress...
+        <Button onClick={() => setViewMode('main')}>X</Button>
+      </Conditional>
     </div>
   );
+}
+interface ItemProp {
+  shouldView: boolean;
+}
+
+const Conditional: React.FC<ItemProp> = ({ shouldView, children }) => {
+  if (shouldView) return <div>{children}</div>;
+  else return null;
+}
+
+interface MenuProps {
+  callback: (option: string) => void;
+  options: string[];
+}
+
+const Menu: React.FC<MenuProps> = (props) => {
+  return <div>{props.options.map(option => <Button onClick={() => props.callback(option)}>{option}</Button>)}</div>;
 }
 
 export default App;
