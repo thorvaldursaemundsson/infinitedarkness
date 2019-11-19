74fe7b5e7075b0b86d16106235f174f933274573
Thorvaldur <thorvaldur.saemundsson@knowit.se>
2019-11-19 16:32:50 +0100

    refactor for simplicity and fixed error

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,22 +1,22 @@
-import * as combat from './skills/combat.json';
-import * as pilot from './skills/pilot.json';
-import * as spells from './skills/spells.json';
-import * as investigation from './skills/investigation.json';
-import * as intimidation from './skills/intimidation.json';
-import * as larceny from './skills/larceny.json';
-import * as stealth from './skills/stealth.json';
-import * as survival from './skills/survival.json';
-import * as science from './skills/science.json';
-import * as computer from './skills/computer.json';
-import * as medicine from './skills/medicine.json';
-import * as surgery from './skills/surgery.json';
-import * as empathy from './skills/empathy.json';
-import * as subtrefuge from './skills/subtrefuge.json';
-import * as athletics from './skills/athletics.json';
-import * as acrobatics from './skills/acrobatics.json';
-import * as search from './skills/search.json';
-import * as slightOfHand from './skills/slightOfHand.json';
-import * as persuation from './skills/persuation.json';
+import combat from './skills/combat.json';
+import pilot from './skills/pilot.json';
+import spells from './skills/spells.json';
+import investigation from './skills/investigation.json';
+import intimidation from './skills/intimidation.json';
+import larceny from './skills/larceny.json';
+import stealth from './skills/stealth.json';
+import survival from './skills/survival.json';
+import science from './skills/science.json';
+import computer from './skills/computer.json';
+import medicine from './skills/medicine.json';
+import surgery from './skills/surgery.json';
+import empathy from './skills/empathy.json';
+import subtrefuge from './skills/subtrefuge.json';
+import athletics from './skills/athletics.json';
+import acrobatics from './skills/acrobatics.json';
+import search from './skills/search.json';
+import slightOfHand from './skills/slightOfHand.json';
+import persuation from './skills/persuation.json';
 
 
 export interface Skill {
@@ -26,122 +26,25 @@ export interface Skill {
     description: string;
 }
 export const GetSkillList = (): Skill[] => {
-
-
     return [
-        {
-            name: combat.name,
-            attribute: combat.attribute,
-            level: combat.level,
-            description: combat.description
-        },
-        {
-            name: pilot.name,
-            attribute: pilot.attribute,
-            level: pilot.level,
-            description: pilot.description
-        },
-        {
-            name: spells.name,
-            attribute: spells.attribute,
-            level: spells.level,
-            description: spells.description
-        },
-        {
-            name: investigation.name,
-            attribute: investigation.attribute,
-            level: investigation.level,
-            description: investigation.description
-        },
-        {
-            name: persuation.name,
-            attribute: persuation.attribute,
-            level: persuation.level,
-            description: persuation.description
-        },
-        {
-            name: intimidation.name,
-            attribute: intimidation.attribute,
-            level: intimidation.level,
-            description: intimidation.description
-        },
-        {
-            name: larceny.name,
-            attribute: larceny.attribute,
-            level: larceny.level,
-            description: larceny.description
-        },
-        {
-            name: stealth.name,
-            attribute: stealth.attribute,
-            level: stealth.level,
-            description: stealth.description
-        },
-        {
-            name: survival.name,
-            attribute: survival.attribute,
-            level: survival.level,
-            description: survival.description
-        },
-        {
-            name: science.name,
-            attribute: science.attribute,
-            level: science.level,
-            description: science.description
-        },
-        {
-            name: computer.name,
-            attribute: computer.attribute,
-            level: computer.level,
-            description: computer.description
-        },
-        {
-            name: medicine.name,
-            attribute: medicine.attribute,
-            level: medicine.level,
-            description: medicine.description
-        },
-        {
-            name: surgery.name,
-            attribute: surgery.attribute,
-            level: surgery.level,
-            description: surgery.description
-        },
-        {
-            name: empathy.name,
-            attribute: empathy.attribute,
-            level: empathy.level,
-            description: empathy.description
-        },
-        {
-            name: subtrefuge.name,
-            attribute: subtrefuge.attribute,
-            level: subtrefuge.level,
-            description: subtrefuge.description
-        },
-        {
-            name: athletics.name,
-            attribute: athletics.attribute,
-            level: athletics.level,
-            description: athletics.description
-        },
-        {
-            name: acrobatics.name,
-            attribute: acrobatics.attribute,
-            level: acrobatics.level,
-            description: acrobatics.description
-        },
-        {
-            name: search.name,
-            attribute: search.attribute,
-            level: search.level,
-            description: search.description
-        },
-        {
-            name: slightOfHand.name,
-            attribute: slightOfHand.attribute,
-            level: slightOfHand.level,
-            description: slightOfHand.description
-        }
+        { ...combat },
+        { ...pilot },
+        { ...spells },
+        { ...investigation },
+        { ...persuation },
+        { ...intimidation },
+        { ...larceny },
+        { ...stealth },
+        {  ...survival },
+        { ...science },
+        { ...computer },
+        { ...medicine },
+        { ...surgery },
+        { ...empathy },
+        { ...subtrefuge },
+        { ...athletics },
+        { ...acrobatics },
+        { ...search },
+        {  ...slightOfHand }
     ].sort((a, b) => a.attribute < b.attribute ? -1 : 1);
 }
