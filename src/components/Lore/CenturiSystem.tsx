80b02470b2810eb09abce6d887186d066070ad79
Thorvaldur <mastikator87@gmail.com>
2020-04-27 20:37:11 +0200

    updated centauri

--- src/components/Lore/CenturiSystem.tsx
+++ src/components/Lore/CenturiSystem.tsx
@@ -9,6 +9,12 @@ const CentauriSystem: React.FC = () => {
     <p>Rigil is the biggest star and has 2 inner planets, it's theorized that if Rigil ever had more planets they would've been slingshotted out of the system from gravitational interaction with Toliman</p>
     <h5>Sheol</h5>
     <p>This small planet orbits Rigil at an extremely low and fast orbit, the planet is mostly covered in molten rock with islands of floating minerals, due to the extreme radiation and heat there is no industry, habitation or outposts</p>
+    <h5>Diluvia</h5>
+    <p>Diluvia is the capitol of the Centauri System, population at nearing 10 million and rapidly increasing, Diluvia was the first colony, chosen for its ideal terrestrial features.</p>
+    <p>60% of the surface of Diluvia is one massive continent spanning the entire planet, the remaining 40% is made up of 29 small seas and hundreds of thousands of lakes, though the seas are salty most have such low salination that the water is drinkable.
+        A complex network of underground rivers connect the seas and allow sea-fauna free reign, many rivers run close to the mantle where minerals from the very active core reach the surface, making the sea water rich.
+    </p>
+    <p>The temperature ranges from -30 to +40 celsius and the seasons are very mild due to very slight 4 degree inclination, the short day night cycle of only 9 hours causes extreme winds.</p>
     
     </>;
 }
