8ac333b85b501de02638a8b144935a9d2cc9a1fa
Thorvaldur <mastikator87@gmail.com>
2020-04-27 20:56:21 +0200

    added centauri stuff

--- src/components/Lore/CenturiSystem.tsx
+++ src/components/Lore/CenturiSystem.tsx
@@ -15,7 +15,28 @@ const CentauriSystem: React.FC = () => {
         A complex network of underground rivers connect the seas and allow sea-fauna free reign, many rivers run close to the mantle where minerals from the very active core reach the surface, making the sea water rich.
     </p>
     <p>The temperature ranges from -30 to +40 celsius and the seasons are very mild due to very slight 4 degree inclination, the short day night cycle of only 9 hours causes extreme winds.</p>
-    
+    <h4>Toliman</h4>
+    <p>Toliman is the middle star, slightly smaller than the sun, has 2 inner planets</p>
+    <h5>Epona</h5>
+    <p>Epona is a rocky planet, no atmosphere and tidally locked to Toliman, one side is an eternal blazing inferno and the other is a fridgig wasteland.
+        However due to the total lack of atmosphere the dark side is covered by snow and ice.
+    </p>
+    <h5>Rhiannon</h5>
+    <p>Rhiannon is a wet terrestrial rockly world with primordial life, vast oceans filled with purple cyanobacteria color the oceans blue at night, the reflection gives the atmosphere a slight purple tint, however it is mostly bright red.
+        Rhiannon is habitable at a cool temperature span of -40 to 10 celsius, the atmosphere has sparse oxygen but otherwise no toxins. Surface gravity 0.9
+    </p>
+    <h4>Proxima Centauri</h4>
+    <p>Proxima is the smallest star orbitting both Rigil Kentaurus and Toliman in a huge orbit at 0.4 lightyears out. FTL travel is needed to travel between but is also much faster.
+        The majority of Centauri planets orbit Proxima but population is sparse.
+    </p>
+    <h5>Hayagriva & Lakshmi</h5>
+    <p>Hayagriva and Laksmi are a twin planetoid pair orbiting close to Proxima, they are tidally locked to each other. Temperatures range from -180 to 300 celsius, surface gravity 0.1 and 0.15 respectively</p>
+    <h5>Sventovid</h5>
+    <p>Sventovid is a partially habitable world, though it is tidally locked to Proxima it is within the habitable zone and is host to life, the day side has a temperature range of 40 - 90, the dark side has a range of -90 - -20.
+        Near the twillight belt is where the climate is most hospitable.
+    </p>
+    <h5>Drega</h5>
+    <p>Drega is a huge icy planet, the extremely thick atmosphere makes landing impossible</p>
     </>;
 }
 
