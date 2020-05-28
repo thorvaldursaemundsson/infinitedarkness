d40742d1ce4a90a98982b6be35fbf4cdba80f4bb
Thorvaldur <thorvaldur.saemundsson@knowit.se>
2020-05-28 12:31:30 +0200

    added some crispy game master guidelines

---
+++ src/components/gamemaster/Guidelines.tsx
@@ -0,0 +1,180 @@
+import React from 'react';
+
+const Guidelines = () =>
+    <>
+        <h1>Guidelines for a game master</h1>
+        <p>These are some guidelines for a game master for various stages of a game.
+        Don't think of them as hard rules but as guidelines, try not to follow them blindly but understand why they're there in the first place.
+            You can ignore them if you have a good reason to think the alternative will lead to a better outcome, but do so at your own peril.</p>
+
+        <h2>Constructing a campaign</h2>
+        <p>These generally concern what happens before session zero and long term planning in general. Some of these also apply when planning or running a session.</p>
+        <ol>
+            <li>Don’t make it flammable if you don’t want it burnt.</li>
+            <li>The DM cannot make the PCs more complex than the players do. No matter what the character sheet says, there are usually only three PC alignments – Lawful Snotty, Neutral Greedy, and Chaotic Backstabbing.</li>
+            <li>What the players want today is a quick, easy victory. But what they will want tomorrow is to have brilliantly and bravely turned the tables to barely survive a deadly encounter where it looked like they were all about to die.</li>
+            <li>Never base a campaign on something you are more excited about than your players are. You may have a great idea for a story based around Andalusian left-handed barbed-wire weaving, but by definition, your players are less interested in it, and less knowledgeable about it, and won't get your clues or references. And they won't care.</li>
+        </ol>
+        <h2>Planning a session</h2>
+        <p>These generally concern what you want to/expect to happen the very next session.</p>
+        <ol>
+            <li>Don't put player characters into a deadly situation unless it is acceptable for the player characters to die.</li>
+            <li>
+                It's your job to build the problem. It's their job to find a solution. If you create a death trap with only one solution, then then they cannot get out unless they figure out what your solution is. But if you build death trap with no solutions, then any clever plan they come up with might work.
+            <ol type='a'>
+                    <li>The purpose of a death trap is not death; it is to make the players feel clever. Don't build one to cause death, and more importantly, don't build one to make them feel stupid.</li>
+                    <li>An escape proof trap, is, by definition, escape proof. What you want is a fool proof trap, and allow your players to amaze you with the quality of fools being made today.</li>
+                    <li>Do not confuse a death trap with no solutions with a death trap that cannot be solved. No resemblance.</li>
+                </ol>
+            </li>
+            <li>
+                Don’t hinge your adventure on the players figuring out a specific clue. Just because it seems obvious to you doesn't mean that it will seem obvious to your players. Have multiple clues, and/or multiple entries.
+                <ol type='a'>
+                    <li>Know what you will do if they never figure out the clue.</li>
+                </ol>
+            </li>
+            <li>
+                Failing to solve the puzzle can cost them hit points, time, resources, curses, some treasure, or surprise attacks, but it should never cost them the adventure.
+                <ol type='a'>
+                    <li> Some nice treasure can be behind the secret door, but the quest object cannot. [Unless there's another way to find it.] </li>
+                </ol>
+            </li>
+            <li>
+                If you aren’t willing for the players to have it, don’t put it in the game. Remember that if the NPC uses an item on the PCs, there are only two possible outcomes:
+                <ol type='a'>
+                    <li>They party will all die, or</li>
+                    <li>The party will wind up with the item.</li>
+                </ol>
+            </li>
+            <li>
+                In every session, each PC should have at least one crucial moment when they are the essential character.
+                <ol type='a'>
+                    <li>Identify the loudest player and the pushiest player. You will never need to set up their moments; they will do so.</li>
+                    <li>Identify the quietest player and the least active player. You will need to set up their moments every session, and make it impossible for the first two to take these moments over.</li>
+                </ol>
+            </li>
+            <li>When a PC gets a great new ability, there needs to be an encounter in the next session for which that ability is devastatingly effective. Otherwise it doesn’t exist. There should also be an encounter in the next session in which it is useless. Otherwise, the rest of that character doesn’t exist.</li>
+            <li>When you design a scenario, you should be firmly on the players' side, trying to produce encounters in which they have every legitimate chance to succeed (and that poor play and bad decisions can still let them fail). But when running the scenario, you need to be a fair and neutral judge of the PCs' actions.</li>
+            <li>
+                You are here to give the players a challenge. But the challenge should be within the game (dragons, traps, puzzles), not playing the game (mapping, tracking equipment).
+                <ol type='a'>
+                    <li>Having said that, they are in still charge of their character sheets and their equipment.</li>
+                </ol>
+            </li>
+        </ol>
+
+        <h2>Running a session</h2>
+        <p>These generally concern what you do during the session.</p>
+        <ol>
+            <li>
+                Never let a player roll a die unless it is reasonable to roll a 20, and reasonable to roll a 1.
+                <ol type='a'>
+                    <li>If there's no way to fail, don't roll. If there's no way to succeed, don't roll.</li>
+                    <li>PCs should not roll for common or obvious knowledge. If the world has three moons, then they don't have to roll to remember it. They've lived under that sky all their life; they don't even have the idea of a world with only one moon.</li>
+                    <li>If it doesn’t matter, then don’t roll dice; summarize. Rolling dice for mop-up combat is as pointless as rolling dice for tying your shoes.</li>
+                </ol>
+            </li>
+            <li>At the start of the game, you should have in mind several ways for the PCs to fail. By contrast, it is not your job to find a way for them to succeed.</li>
+            <li>Reward good tactics, consistent characterization, and brilliant ideas more than lucky die rolls.</li>
+            <li>A role-playing game is run by rules. But it isn't made out of rules; it's made out of ideas, characters, and imagination.</li>
+            <li>
+                The more completely you know the rules, the better you can be at ignoring them when necessary.
+                <ol type='a'>
+                    <li>"When necessary" means it should be rare, forced by an unusual situation, and non-intrusive. [And some people believe it should not happen even then.]</li>
+                    <li>Applying the rules is like eating food. That should always happen. Ignoring the rules is like taking medicine; it's only a good idea if something is wrong.</li>
+                    <li>Never change a rule unless you know why it was written.</li>
+                </ol>
+            </li>
+            <li>
+                The players do not have the right to screw up the game. They do have the right to screw up your plot. Don’t confuse the two.
+                <ol type='a'>
+                    <li>Do not give them a set of options that includes screwing up the game.</li>
+                    <li>“Screwing up the game” includes genre-busting. Medieval fantasies don’t have railroads, factories, or atomic power; the players have no right to introduce them (unless genre-busting is a focus for that game).</li>
+                </ol>
+            </li>
+            <li>
+                The dice do not have the right to screw up the game. They do have the right to screw up your plot. Don’t confuse the two
+                <ol type='a'>
+                    <li>Do not roll a die if the result could screw up the game</li>
+                </ol>
+            </li>
+            <li>
+                The DM does not have the right to screw up the PC's story. He does have the right to screw up the PCs' plans. Don’t confuse the two.
+                <ol type='a'>
+                    <li>The player does have the right to screw up the PC's story -- even by accident. If a 2nd level PC chooses to attack a dragon, then the PC's death is his doing, not the DM's.</li>
+                </ol>
+            </li>
+            <li>
+                There are players who see the world as a series of activities they can safely and straightforwardly defeat, and there are players who see the world as a dangerous world with life-threatening risks behind every bush. You cannot run the same game for both sets. Neither is inherently bad, but know which kind of players you have.
+                <ol type='a'>
+                    <li>If the term "CR" is a common part of the players' conversation, assume that you have the first group, and plan accordingly. Never count on them deciding to run away from an encounter.</li>
+                </ol>
+            </li>
+            <li>When the players ask for something - an item, a skill, a feat, whatever - they are not planning to use it for what it is intended for, they're planning the weirdest thing it could possibly be used for. [If they were planning to use it as intended, then they wouldn’t bother to ask.]</li>
+            <li>A backstory is like a sword. Some characters are incomplete without one, and others wouldn't know how to use one even if they had it.</li>
+            <li>A player's backstory isn't your toy to destroy if you want; it's part of their toy. You can threaten their friends, family, or homes, but by the end of the adventure, the players should not feel abused. Use their family as hostages, but expect them to be rescued, and to come home with more than they started with. If you burn down their cottage, they should wind up with a castle. The players should be glad that the backstory was used, not sorry that they had a background.</li>
+            <li>
+                A game is a co-operative venture. You don't have the right to force players into your game against their will, and for the same reasons, players don't have the right to force themselves into your game against your will.
+                <ol type='a'>
+                    <li>Not all games are alike, and that's fine. Not all players want the same things out of a game, and that's fine.</li>
+                    <li> Avoid having players who won't like the kind of game you're running. And then run a game your players will enjoy.</li>
+                </ol>
+            </li>
+            <li>
+                The purpose of wandering monsters is to prevent the game from bogging down. If the players spend over five real minutes in useless discussion, then it's ghoul o'clock.
+                <ol type='a'>
+                    <li>Be careful with this. Not all discussion is useless.</li>
+                    <li>There should be encounters that have nothing to do with the main quest, or there is no world – just a party and a quest.</li>
+                </ol>
+            </li>
+            <li>
+                When the party’s victory is assured, the encounter has lost all suspense. Mop-up combat is boring, so end it.
+                <ol type='a'>
+                    <li>Remember, the NPCs don’t want to die; they would usually rather flee, negotiate, or surrender.</li>
+                    <li>One round earlier, when you know the PCs have won and they don’t yet, is a great time for the NPCs to offer to negotiate.</li>
+                    <li>This is your opportunity to force-feed them that obvious fact they’ve been missing, and let them believe they earned it.</li>
+                </ol>
+            </li>
+            <li>
+                As far as possible, interact with the characters, not the players.
+                <ol type='a'>
+                    <li>The player is asking about the rules. The character is asking about the orcs. Don’t confuse the two.</li>
+                </ol>
+            </li>
+            <li>
+                Don't plan how to answer a player's question until you know what the question is. And don't approach all players the same.
+                <ol type='a'>
+                    <li>The player who asks for nonsense should most often get a "No". The player whose requests are pretty basic and reasonable should most often get a "Yes". The player who asks for something cool and cinematic, but unlikely, requires a careful judgment call. She should sometimes get it -- but rarely enough that it creates a climactic moment, not an average move.</li>
+                    <li>Think about a movie where you've seen something like this happen. Did the hero do it often? Probably the player should be allowed to do it often. Did the hero do it once, as a desperate move, at the big finish? Then save it for the big finish.</li>
+                </ol>
+            </li>
+            <li>
+                The player identifies with the PC, and will take what happens to the PC personally. If the PC wants to defeat the orcs, then the player wants to defeat the orcs. The DM does not have that luxury. The orcs want to kill the PCs, but the DM should not.
+                <ol type='a'>
+                    <li>Follow Matt Dillon’s principle: “I never hang anybody. The law does.” The DM should never kill a PC. Sometimes the game might.</li>
+                </ol>
+            </li>
+            <li>
+                When the players come up with something you never considered, stop and think. This is the source of your absolute best, most perfect moments. It’s also where all scenario-destroying mistakes come from. Ask yourself which it is before you react.
+                <ol type='a'>
+                    <li>"Scenario-ending" and "scenario-destroying" are not (necessarily) synonyms. You may have planned a major battle in front of the black Gate. But a PC endings the quest by throwing the Ring into the Cracks of Doom could still be a satisfying, if abrupt, ending.</li>
+                </ol>
+            </li>
+            <li>
+                Remind them of things that their characters would not have forgotten, but not things that characters will forget.
+                <ol type='a'>
+                    <li>The PCs can’t forget that they picked up a magic glaive, so if they start looking around for a long weapon, remind them that they have it. And they won’t forget the face of the sorceress who destroyed their village. But if they forgot that the blacksmith said he heard about ogres in the hills, then the PCs weren’t paying attention.</li>
+                    <li>This can require some careful judgment calls.</li>
+                </ol>
+            </li>
+            <li>
+                You will make mistakes -- lots of them. A crucial skill to be a good DM is the ability to fix mistakes and as quickly and seamlessly as possible.
+                <ol type='a'>
+                    <li>This will sometimes involve admitting them. It will also sometimes involve keeping the players from ever seeing them.</li>
+                    <li>In either case, the point is to make the game go forward, not to repair your ego.</li>
+                </ol>
+            </li>
+        </ol>
+    </>;
+
+export default Guidelines;
--- src/views/GameMaster.tsx
+++ src/views/GameMaster.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Section from '../components/playermanual/Section';
-import Sequencer from '../components/gamemaster/Sequencer';
+import Guidelines from '../components/gamemaster/Guidelines';
+
 
 const GameMaster: React.FC = () => {
     return (<div>
         <h1>Game Master</h1>
         <p>This holds a collection of tools for the game master</p>
-        <Section title='Sequencer'>
-            <Sequencer />
+        <Section title='Guidelines'>
+            <Guidelines />
         </Section>
     </div>);
 }
