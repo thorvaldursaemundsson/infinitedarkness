b9026dcc7f10556fe662c4a218461998179c7030
Thorvaldur <thorvaldur.saemundsson@knowit.se>
2020-10-12 20:02:52 +0200

    nerfed end of session exp

--- src/components/gamemaster/Experience.tsx
+++ src/components/gamemaster/Experience.tsx
@@ -10,15 +10,15 @@ const Experience: React.FC = () => {
         <p>Note that it's okay to change your character's personality traits if you have a good reason, but the game master must know.</p>
         <ul>
             <li>+0 If the player directly contradicts their traits</li>
-            <li>+1 If the player neither contradicts or adheres to their trait</li>
-            <li>+2 If the player plays according to their traits most of the time and doesn't contradict them</li>
-            <li>+3 If the player plays according to their traits exceptionally well</li>
+            <li>+0.5 If the player neither contradicts or adheres to their trait</li>
+            <li>+1 If the player plays according to their traits most of the time and doesn't contradict them</li>
+            <li>+2 If the player plays according to their traits exceptionally well</li>
         </ul>
         <h5>Overcoming adversity</h5>
         <p>If the character overcomes a huge challenge then they should be awarded +1. This can be anything from stopping a bomb from going off, pulling a survivor out of a burning building, defending against pirate attack or becoming elected mayor.</p>
         <h5>MVP Nomination</h5>
         <p>Finally, at the end of the game each player should nominate one other player as their nominee. Rather than announcing out load each player should write down the name of the player, and then read from the note when everyone is done.</p>
-        <p>Each nomination grants a +1 experience point, it's preferred that the players choose based on characterization, cooperation and making the game fun for everyone.</p>
+        <p>Each nomination grants a +1 experience point, it's preferred that the players choose based on characterization, cooperation and making the game fun for everyone. However the players get the final say on who they give this experience to and they may do whatever they want.</p>
         <p>This is meant to encourage gratitute and cooperation among the players</p>
         <h4>Experience Multiplier</h4>
         <p>When the sources are all added up, each character multiplies their end of session experience and adds it to their pool of unsused character points.</p>
